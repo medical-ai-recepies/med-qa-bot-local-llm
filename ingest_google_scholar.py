84cbf1cc7d675b6a3fee0b949c122b84860e4257
Dasa Ponnappan <dasa@manizh.com>
2024-06-29 00:04:08 -0400

    Checking in changes after removing secrets

--- ingest_google_scholar.py
+++ ingest_google_scholar.py
@@ -23,12 +23,10 @@ local_path = (
 )
 local_llm = "meditron-7b.Q4_K_M.gguf"
 # get the API key from the github secrets
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 # assign it to the environment variable
-# os.environ["OPENAI_API_KEY"] = openai.api_key
-os.environ["OPENAI_API_KEY"] = (
-    "sk-proj-XbUsVEqAZDhW9je90anIT3BlbkFJasvVa7C2sPaCd8cWC8ek"
-)
+os.environ["OPENAI_API_KEY"] = openai.api_key
+
 # local_llm = GPT4All(model=local_path, verbose=True)
 embeddings = SentenceTransformerEmbeddings(
     model_name="NeuML/pubmedbert-base-embeddings"
